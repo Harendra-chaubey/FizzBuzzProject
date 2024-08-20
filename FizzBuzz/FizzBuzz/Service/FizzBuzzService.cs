42b7154805e7afe94048d17d77bacf77fb9f4002
Harendra-chaubey <76199162+Harendra-chaubey@users.noreply.github.com>
2024-08-20 15:22:56 +0530

    Upload updated
    upload again after updated as per need the changes

--- FizzBuzz/FizzBuzz/Service/FizzBuzzService.cs
+++ FizzBuzz/FizzBuzz/Service/FizzBuzzService.cs
@@ -3,13 +3,28 @@ namespace FizzBuzz.Service
 {
     public class FizzBuzzService : IFizzBuzzService
     {
+        private const int DivisorThree = 3;
+        private const int DivisorFive = 5;
         public string GetFizzBuzzResult(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0) return "FizzBuzz";
-            else if (number % 3 == 0) return "Fizz";
-            else if (number % 5 == 0) return "Buzz";
-            else return "Invalid item.";           
-          
+            if (number % DivisorThree == 0 && number % DivisorFive == 0)
+            {
+                return "FizzBuzz";
+            }
+            else if (number % DivisorThree == 0)
+            {
+                return "Fizz";
+            }
+            else if (number % DivisorFive == 0)
+            {
+                return "Buzz";
+            }
+            else
+            {
+                // Return the division attempts as required
+                return $"Divided {number} by {DivisorThree}\nDivided {number} by {DivisorFive}";
+            }
+
         }
     }
 }
